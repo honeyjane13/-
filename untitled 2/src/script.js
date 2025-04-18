0b1e5e92b265f2aa79659c0fdfef334c502a1415
honeyjane13 <caodoudou1733@gmail.com>
2025-04-18 21:58:57 +0800

    Delete untitled 2 directory

--- untitled 2/src/script.js
+++
@@ -1,7 +0,0 @@
-function logEntry() {
-  const logList = document.getElementById("log-list");
-  const li = document.createElement("li");
-  const now = new Date().toLocaleString();
-  li.innerText = "打卡时间：" + now;
-  logList.appendChild(li);
-}
